66781a71dc3e1d0b5c9664b8c3a56284cc29ae21
AnReXx <anrid040@gmail.com>
2024-07-29 16:54:19 +0400

    second commit

--- main.js
+++ main.js
@@ -1,21 +1,5 @@
 const items = document.querySelectorAll(".pathway_containers .accordion-item");
 
-/**
-let sections = document.querySelectorAll("section");
-window.onscroll = () => {
-  sections.forEach((sec) => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop;
-    let height = sec.offsetHeight;
-
-    if (top >= offset && top < offset + height) {
-      sec.classList.add("show-animate");
-    } else {
-      sec.classList.remove("show-animate");
-    }
-  });
-};**/
-
 // scroll-animation
 let sections = document.querySelectorAll("section");
 window.onscroll = () => {
